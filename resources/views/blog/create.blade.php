c88a2148268d249caf1cc63ff60c7c917dfe9e0e
Beemons <beemons123@gmail.com>
2019-01-31 02:55:57 +0700

    Mengatur Validasi dan nilai sebelumnya

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -3,18 +3,17 @@
 @section('title', 'Edit page')
 
 @section('content')
-  @if(count($errors) > 0)
-    <ul>
-      @foreach($errors->all() as $error)
-        <li> {{ $error }}</li>
-      @endforeach
-    </ul>
-  @endif
     <h1>Create Blog </h1>
 
     <form action="/laravel1/bloggy/public/blog" method="post">
-        <input type="text" name="title" value=""> <br>
-        <textarea name="description" rows="8" cols="80"></textarea> <br>
+        <input type="text" name="title" value="{{ old('title') }}"> <br>
+        @if($errors->has('title'))
+            <p>{{ $errors->first('title') }}</p>
+        @endif
+        <textarea name="description" rows="8" cols="80"> {{ old('description') }}</textarea> <br>
+        @if($errors->has('description'))
+            <p>{{ $errors->first('description') }}</p>
+        @endif
         <input type="submit" name="submit" value="Create">
         {{ csrf_field() }}
     </form>
