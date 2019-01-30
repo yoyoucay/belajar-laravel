684f5f69385b9b96cbbcf74b808aa76d3738a022
Beemons <beemons123@gmail.com>
2019-01-30 14:08:59 +0700

    Database dengan query Builder

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -14,9 +15,35 @@ class BlogController extends Controller
     public function show($id)
     {
         $nilai = "Ini adalah linknya ".$id;
-        $user = "Arif Febriana";
+
+/* QUERY Create , Update , Delete */
+        // insert queries
+        // DB::table('users')->insert([
+        //   ['username' => 'Arif Febriana', 'password' => '123456']
+        // ]);
+
+        // Update Queries
+        // DB::table('users')->where('id', '3')->update(
+        //   ['username' => 'Arif Ganteng', 'password' => 'lala123']
+        // );
+
+        // Delete queries
+        // DB::table('users')->where('id', '>' , '2')->delete();
+        // Queri ini akan menghapus akun yang ID nya lebih dari 2.
+
+
+/* QUERY untuk menampilkan data */
+
+        // Menampilkan semua data pada table Users
+        $users = DB::table('users')->get();
+
+        // Menampilkan data Lebih spesifik berdasarkan Username
+        // $users = DB::table('users')->where('username', 'arif')->get(); menggunakan query builder WHERE
+
+        // laravel.com/docs/5.3/queries for more info about Queries
+
+        // dd($users); fungsinya sama seperti die(var_dump())
         $unescape = '<script>alert("WARNINGS!")</script>';
-        $job = ['Back-end', 'Mahasiswa terbgsd', 'Coolest man', 'ngeue'];
-        return view('blog/single', ['blog' => $nilai, 'user' => $user, 'pekerjaan' => $job, 'unescape' => $unescape]);
+        return view('blog/single', ['blog' => $nilai, 'users' => $users, 'unescape' => $unescape]);
     }
 }
--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,21 +5,20 @@
 @section('content')
     <h1>Ini adalah halaman Biodata </h1>
     <h3>{{$blog}}</h3>
-    <h4>{{$user}}</h4>
 
-    @foreach($pekerjaan as $hobby)
-        <li>{{ $hobby }}</li>
+    @foreach($users as $user)
+        <li>{{ $user->username.' '.$user->password }}</li>
     @endforeach
-    
-    {!! $unescape !!} 
+
+    <!-- {!! $unescape !!} -->
     {{ $unescape }}
 
     <div>
-    @if(count($pekerjaan) > 2)
+    @if(count($users) > 2)
         <p>wah banyak juga ya</p>
     @else
         <p> lumayan banyak </p>
     @endif
     </div>
-    
+
 @endsection
